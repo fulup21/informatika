87cec6bb5f6e59fee636c59673ddd34d7ebbd69f
fulup21 <dzupinf@arcig.cz>
2023-10-14 22:15:42 +0200

    verze 2

--- prumer.py
+++ prumer.py
@@ -1,2 +1,17 @@
 import sys
 
+cislo = sys.argv
+total = 0
+#print(cislo)
+pocetcisel = 0
+
+for e in cislo:
+    try:
+        #print(e)
+        total += int(e)
+        pocetcisel = pocetcisel + 1
+    except:
+        print(f' {e} neni cele cislo!!!!!')
+
+
+print(f'Prumer je {total/pocetcisel}')
